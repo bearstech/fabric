2148afe91bbda5503912f4a37bf4ec5cfab338f9
Charpentier Johan <jcharpentier@bearstech.com>
2011-03-01 19:43:52 +0100

    Add ssh-agent capabilities with patched paramiko

--- fabric/operations.py
+++ fabric/operations.py
@@ -729,6 +729,10 @@ def _execute(channel, command, pty=True, combine_stderr=True,
             rows, cols = _pty_size()
             channel.get_pty(width=cols, height=rows)
 
+        # request agent
+        from paramiko.agent import AgentClientProxy
+        forward = AgentClientProxy(channel)
+
         # Kick off remote command
         if invoke_shell:
             channel.invoke_shell()
